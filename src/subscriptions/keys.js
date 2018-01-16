29cb536d940afcf755e2426565bf8f16414e3913
Lukasz <lukaszkochajews@gmail.com>
2018-01-16 13:39:43 +0100

    stop tracking keys

--- src/subscriptions/keys.js
+++
@@ -1,4 +0,0 @@
-export default {
-  vapidPublicKey: 'BFRKDS3jmAA8Qp_SQEiKMPGK7E_Kdnd_6N_x-kkeK-I7EK3V05Yro-0V1_JxBixqpXokY3VZ6PfQtpRQOoAS5-4',
-  vapidPrivateKey: 'XTrOYVjN706SVI2phLvnu_2YkPGIHchrSIEHmsZbq44'
-}
